2c901fec6c41fd2eb0ac492c7dff42842b8b2166
BIGFLATNOW <97323733+BIGFLATNOW@users.noreply.github.com>
2022-01-08 03:43:23 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,50 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
+  <form 
+action="https://formsubmit.co/5936f3264e1b3101d767692f779ca23a" method="POST">
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Spectral');
+
+input {
+width: 54%;
+height: fit-content;
+font-family:'Spectral';
+font-size:180%;
+padding: 5px 10px}
+
+button {
+width: 45%;
+height: fit-content;
+font-family:'Spectral';
+font-size:180%;
+padding: 5px 0px}
+</style> 
+
+
+<input 
+type="email" 
+name="email" placeholder="Email" required>
+
+<input 
+type="hidden" 
+name="_subject" 
+value="Neue Abonennt*in!">
+
+<input 
+type="hidden" 
+name="_captcha" 
+value="false">
+
+<button 
+type="submit">
+<u>Abonnieren</u>
+</button>
+
+</form>
         Unterstützen:    paypal.me/GEFAENGNISHEFTE
 Kontakt:    BIGFLATNOW@gmail.comDeabonnieren    |    Impressum
       </footer>
