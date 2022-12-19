0931217d4549da2a08ef9fcc7096aa6657ffb8b8
BIGFLATNOW <bigflatnow@gmail.com>
2022-12-19 08:45:13 +0100

    upload

--- components/header.js
+++ components/header.js
@@ -2,25 +2,7 @@
 //////////////////////////////////////////////////// GENERAL ////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-var getURL = function (url, success, error) {
-	if (!window.XMLHttpRequest) return;
-	var request = new XMLHttpRequest();
-	request.onreadystatechange = function () {
-		if (request.readyState === 4) {
-			if (request.status !== 200) {
-				if (error && typeof error === 'function') {
-					error(request.responseText, request);
-				}
-				return;
-			}
-			if (success && typeof success === 'function') {
-				success(request.responseText, request);
-			}
-		}
-	};
-	request.open('GET', url);
-	request.send();
-};
+
 
 // HEIGHTFIX
 
