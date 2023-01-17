47d71f3625200336be347491847d3ca21369b22a
BIGFLATNOW <bigflatnow@gmail.com>
2023-01-17 06:03:24 +0100

    Upload

--- components/common.js
+++ components/common.js
@@ -142,13 +142,13 @@ function init_footer() {
 
 // HEIGHTFIX
 
-// let vh = window.innerHeight * 0.01;
-// document.documentElement.style.setProperty('--vh', `${vh}px`);
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-// window.addEventListener('resize', () => {
-// 	let vh = window.innerHeight * 0.01;
-// 	document.documentElement.style.setProperty('--vh', `${vh}px`);
-// });
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+	document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
 
 
 // SCROLLBAR FOR INDEX
